ca4ce265e90b1dfabf53ce4b4040dfbf413ef767
Jeongho_Park <possuntix@gmail.com>
2026-08-03 21:50:59 +0900

    docs: solo_status 라벨을 '1인석 있음 (확인방법)' 형태로 명확화

--- lib/soloStatus.ts
+++ lib/soloStatus.ts
@@ -1,3 +1,3 @@
-export const SOLO_STATUS_VALUES = ['네이버 지도 정보', '직접 확인', '미확인'] as const
+export const SOLO_STATUS_VALUES = ['1인석 있음 (네이버 지도 정보)', '1인석 있음 (직접 확인)', '미확인'] as const
 
 export type SoloStatus = (typeof SOLO_STATUS_VALUES)[number]
